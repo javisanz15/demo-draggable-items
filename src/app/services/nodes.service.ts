61ba7daca8d2222b170e639487f8df2ec48f2ba5
Javier Sanz <javitosanz15@gmail.com>
2020-01-21 12:00:24 +0100

    Rollback to the first version

--- src/app/services/nodes.service.ts
+++ src/app/services/nodes.service.ts
@@ -6,65 +6,40 @@ import { Observable, of } from 'rxjs';
 })
 export class NodesService {
 
-public nodesToShow = {
-  raw: [
-    {
-      label: 'Policy Identifier',
-      id: 'policyIdentifier'
-    },
-    {
-      label: 'Starting Date',
-      id: 'startingDate'
-    },
-    {
-      label: 'Policy Number',
-      id: 'policyNumber'
-    },
-    {
-      label: 'Party ID',
-      id: 'partyId'
-    },
-  ],
-  linkers: [
-    {
-      label: 'Rule #1',
-      id: 'rule1'
-    },
-    {
-      label: 'Rule #2',
-      id: 'rule2'
-    },
-    {
-      label: 'Rule #3',
-      id: 'rule3'
-    },
-  ],
-  target: [
-    {
-      label: 'Policy ID',
-      id: 'policyId',
-      icon: 'device_hub'
-    },
-    {
-      label: 'Start Date',
-      id: 'startDate',
-      icon: 'satellite'
-    },
-    {
-      label: 'Policy Number',
-      id: 'policyNum',
-      icon: 'satellite'
-    },
-    {
-      label: 'Party',
-      id: 'party',
-      icon: 'device_hub'
-    },
-  ]
-};
+public newNode = {
+  id: 'card-5',
+  icon: 'contactless',
+  label: 'Term #5'
+}
+
+public nodesToShow = [
+  {
+    id: 'card-1',
+    icon: 'fingerprint',
+    label: 'Term #1'
+  },
+  {
+    id: 'card-2',
+    icon: 'eco',
+    label: 'Term #2'
+  },
+  {
+    id: 'card-3',
+    icon: 'extension',
+    label: 'Term #3'
+  },
+  {
+    id: 'card-4',
+    icon: 'account_balance',
+    label: 'Term #4'
+  },
+];
 
 constructor() { }
 
+public getNewNode(): Observable<any> {
+  return of(this.newNode);
+}
 
 public getNodesToShow(): Observable<any> {
   return of(this.nodesToShow);
